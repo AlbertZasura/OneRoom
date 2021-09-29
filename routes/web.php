7c56651403cfed9707380c8bef43a2c6ca1b19de
AlbertZasura <albertalbert841@gmail.com>
2021-09-29 21:53:22 +0700

    add show controller for messages

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/messages', [MessageController::class, 'index']);
-
+// Route::get('/messages', [MessageController::class, 'index']);
+Route::resource('messages', MessageController::class);
