4e57712e83c1041a05c105fef3429435f085fbb3
helenaawijaya <74819954+helenaawijaya@users.noreply.github.com>
2021-10-03 21:06:42 +0700

    index_show_blade

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -1,48 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pengumuman</title>
-</head>
-<body>
+@extends('Layout.SidePanel')
+
+@section('content')
     <h1>Pengumuman</h1>
-    <table>
-        <tr>
-            <th style="width:25%"></th>
-            <th style="width:25%"></th>
-            <th style="width:25%"></th>
-        </tr>
+    @if (Auth::user())
+    <form action="/logout" method="POST">   
+        @csrf
+        <button type="submit" class="btn btn-danger">Logout</button>
+    </form>
+    @else
+    <a class="btn btn-info" href="/login">Login</a>  
+    @endif
+
+    <table style="width:35%">
         <tr>
             <td>
-                <a class="card-text">Pengirim :</a> 
+                <a>Pengirim</a> 
+            </td>
+            <td>
+                <a>{{ Auth::user()->name }}</a>
                 <br>
             </td>
-            <td>
-                <a>:</a>
-            </td>
-                
-            <td>
-                <a class="card-text">{{ Auth::user()->name }}</a>
-            </td>
         </tr>
         <tr>
-        </td>
             <td>
-                <a class="card-text">Tanggal : </a> <a class="card-text">{{ $message->created_at->format('d M Y') }}</a>
+                <a>Tanggal</a>
+            </td>
+            <td>
+                <a>{{ $message->created_at->format('d M Y') }}</a>
                 <br>
             </td>
             
         </tr>
         <tr>
             <td>
-                <a class="card-text">Judul : </a> <a class="card-text">{{ $message->title }}</a>
+                <a>Judul</a>
+            </td>
+            <td>
+                <a>{{ $message->title }}</a>
                 <br>
             </td>
         </tr>
     </table>
     <br>
-    <a class="card-text"> {{ $message->content }}</a>
-</body>
-</html>
+    <div class="card" style="width: 100%; height: 50%;">
+        <div class="card-body">
+            <a> {{ $message->content }}</a>
+        </div>
+    </div>
+@stop
