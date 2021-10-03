a2ecec9eac234918e3b129138ebdf6c615e7afbb
kadek-ar <ikadek@indotrding.com>
2021-10-03 15:35:21 +0700

    Update SidePanel.blade.php

--- resources/views/Layout/SidePanel.blade.php
+++ resources/views/Layout/SidePanel.blade.php
@@ -7,6 +7,7 @@
     <title>Learning Management System</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/6538af5efe.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <style>
         .w-50{
             width: 50%;
