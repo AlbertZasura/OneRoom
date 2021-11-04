ba56230cbd154042ef5804cf0e1444216dcd9cef
AlbertZasura <albertalbert841@gmail.com>
2021-11-05 06:54:00 +0700

    fix modal

--- resources/views/authentications/_role_modal.blade.php
+++ resources/views/authentications/_role_modal.blade.php
@@ -3,15 +3,14 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header border-bottom-0">
-            {{-- <h5 class="modal-title" id="exampleModalLabel">Modal title</h5> --}}
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body text-center">
-                <h3>Daftar Sebagai</h3>
+                <h3 class="modal-title mb-3">Daftar Sebagai</h3>
                 <a href="/register/teacher" class="btn btn-warning">Guru</a>
-                <p>Atau</p>
+                <p class="my-1 m-0">Atau</p>
                 <a href="/register" class="btn btn-warning">Siswa</a>
-                <p>Atau</p>
+                <p class="my-1 m-0">Atau</p>
                 <a href="/register/admin" class="btn btn-warning">Admin</a>
                 </div>
             </div>
