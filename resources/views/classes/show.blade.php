972a535bbf2fc4f92690e0eb63a5daeb38edabdb
AlbertZasura <albertalbert841@gmail.com>
2021-11-14 21:35:09 +0700

    add font

--- resources/views/classes/show.blade.php
+++ resources/views/classes/show.blade.php
@@ -4,61 +4,65 @@
 @section('title', "Kelas {$class->name} | OneRoom")
 
 @section('show')
-    <div class="d-flex align-items-center">
-        @can('delete', $class )
-            <form action="{{ route('classes.destroy',$class) }}" method="POST">   
-                @csrf
-                @method('DELETE')      
-                <button class="btn" type="submit" onclick="return confirm('Apakah Anda yakin untuk menghapus kelas ini?')"><i class='fs-25 fa fa-trash text-danger'></i></button>
-            </form>
-        @endcan
-        <h1 class="text-uppercase">{{$class->name}}</h1>
-        {{-- @can('update',$class )
-            <a href="{{ route('classes.edit',$class->id) }}" class="btn"><i class='fs-25 fa fa-pencil text-primary'></i></a>
-        @endcan --}}
-    </div>
-    @can('user_list', App\Models\Classes::class )
-        <a href="/classes/{{$class->id}}/assign_user" class="btn btn-outline-green float-end">
-            <i class='fa fa-plus '></i> Tambah Anggota
-        </a>
-    @endcan
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Nama</th>
-                <th>Jabatan</th>
-                @can('assign_user', App\Models\Classes::class )
-                    <th>Aksi</th>
+    <div class="card-shadow bg-white rounded-3">
+        <div class="card-body">
+            <div class="d-flex align-items-center">
+                @can('delete', $class )
+                    <form action="{{ route('classes.destroy',$class) }}" method="POST">   
+                        @csrf
+                        @method('DELETE')      
+                        <button class="btn" type="submit" onclick="return confirm('Apakah Anda yakin untuk menghapus kelas ini?')"><i class='fs-25 fa fa-trash text-danger'></i></button>
+                    </form>
                 @endcan
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($users as $key => $user )
-                <tr>
-                    <th>
-                        <p>{{ $key+1 }}.</p> 
-                    </th>
-                    <td>
-                        <p>{{ $user->name }}</p> 
-                    </td>
-                    <td>
-                        <p>
-                            {{ $user->role }} <br>
-                            {{ $user->course($class->id)->pluck('name')->implode(', ') }}
-                        </p> 
-                    </td>
-                    @can('assign_user', App\Models\Classes::class )
-                        <td>
-                            <form action="/classes/{{$class->id}}/assign_user/{{$user->id}}?type=detach" method="POST">
-                                @csrf
-                                <button type="submit" class="btn btn-outline-green rounded-pill" onclick="return confirm('Apakah Anda yakin untuk mengeluarkan {{ $user->name }} dari kelas?')">Keluarkan</button>
-                            </form>
-                        </td>
-                    @endcan
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-    {{ $users->links() }}
+                <h1 class="text-uppercase">{{$class->name}}</h1>
+                {{-- @can('update',$class )
+                    <a href="{{ route('classes.edit',$class->id) }}" class="btn"><i class='fs-25 fa fa-pencil text-primary'></i></a>
+                @endcan --}}
+            </div>
+            @can('user_list', App\Models\Classes::class )
+                <a href="/classes/{{$class->id}}/assign_user" class="btn btn-outline-green float-end">
+                    <i class='fa fa-plus '></i> Tambah Anggota
+                </a>
+            @endcan
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>Jabatan</th>
+                        @can('assign_user', App\Models\Classes::class )
+                            <th>Aksi</th>
+                        @endcan
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $key => $user )
+                        <tr>
+                            <th>
+                                <p>{{ $key+1 }}.</p> 
+                            </th>
+                            <td>
+                                <p>{{ $user->name }}</p> 
+                            </td>
+                            <td>
+                                <p>
+                                    {{ $user->role }}
+                                    {{ $user->course($class->id)->pluck('name')->implode(', ') }}
+                                </p> 
+                            </td>
+                            @can('assign_user', App\Models\Classes::class )
+                                <td>
+                                    <form action="/classes/{{$class->id}}/assign_user/{{$user->id}}?type=detach" method="POST">
+                                        @csrf
+                                        <button type="submit" class="btn btn-outline-green rounded-pill" onclick="return confirm('Apakah Anda yakin untuk mengeluarkan {{ $user->name }} dari kelas?')">Keluarkan</button>
+                                    </form>
+                                </td>
+                            @endcan
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            {{ $users->links() }}
+        </div>
+    </div>
 @endsection
--- resources/views/contents/index.blade.php
+++ resources/views/contents/index.blade.php
@@ -4,32 +4,36 @@
 @section('title', 'Konten | OneRoom')
 
 @section('content')
-    <h1>Konten</h1>
-    <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">No</th>
-            <th scope="col">Nama</th>
-            <th scope="col">Isi</th>
-            <th scope="col">Perubahan Terakhir</th>
-            <th scope="col">Aksi</th>
-          </tr>
-        </thead>
-        <tbody>
-            @foreach ($contents as $key=>$content )
+    <div class="card-shadow bg-white">
+        <div class="card-body">
+            <h1>Konten</h1>
+            <table class="table">
+                <thead>
                 <tr>
-                    <th scope="row">{{ $key+1 }}</th>
-                    <td>{{ $content->name }}</td>
-                    <td>{{ $content->value }}</td>
-                    <td>{{ $content->updated_at }}</td>
-                    <td>
-                        <a data-bs-toggle="modal" data-bs-target="#editContents{{ $content->id }}" class="btn btn-fill-green rounded-pill">
-                            <i class='fa fa-pen'></i> Ubah
-                        </a>
-                        @include('contents._edit')
-                    </td>
+                    <th scope="col">No</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Isi</th>
+                    <th scope="col">Perubahan Terakhir</th>
+                    <th scope="col">Aksi</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+                </thead>
+                <tbody>
+                    @foreach ($contents as $key=>$content )
+                        <tr>
+                            <th scope="row">{{ $key+1 }}</th>
+                            <td>{{ $content->name }}</td>
+                            <td>{{ $content->value }}</td>
+                            <td>{{ $content->updated_at }}</td>
+                            <td>
+                                <a data-bs-toggle="modal" data-bs-target="#editContents{{ $content->id }}" class="btn btn-fill-green rounded-pill">
+                                    <i class='fa fa-pen'></i> Ubah
+                                </a>
+                                @include('contents._edit')
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>   
 @endsection
