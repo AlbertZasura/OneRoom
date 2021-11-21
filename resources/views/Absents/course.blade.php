582fa23622f2100d99c8cba94c0d0641b08dad36
kadek-ar <ikadek@indotrding.com>
2021-11-21 21:15:30 +0700

    fix

--- resources/views/Absents/course.blade.php
+++ resources/views/Absents/course.blade.php
@@ -4,16 +4,17 @@
 @section('title', 'Absen | OneRoom')
 
 @section('content')
-    <h1>Absen</h1>
-    <div class="d-flex">
-        <div class="w-200px">
+    <h1 class="show-on-dekstop">Absen</h1>
+    <div class="d-flex mobile-overflow-hidden mobile-w-100">
+        <div id="cardMenu" class="mobile-card-menu mobile-w-100 w-200px">
+            <h1 class="show-on-mobile">Absen</h1>
             @can('viewAny', App\Models\Absent::class )
                 @foreach ($courses as $c )
-                <div class="card {{ ($c->id===request('course_id')) ? "bg-biru-muda color-hijau-tua"  : "bg-hijau-tua text-white" }} mb-3">
+                <div class="card {{ ($c->id===request('course_id')) ? " color-hijau-tua"  : "bg-hijau-tua text-white" }} mb-3">
                     <div class="card-body">
-                        <h5 class="card-title">{{$c->name}}</h5>
+                        <div class="text-capitalize fs-16">{{$c->name}}</div>
                         <a href="{{route('course.absents.index',$c)}}" class="stretched-link"></a>
-                        <p class="card-text text-end">
+                        <p class="card-text text-end fs-14 mt-2">
                             {{ App\Models\Schedule::where('course_id',$c->id)->where('class_id',Auth::user()->classes->first()->id)->count() }} Pertemuan
                         </p>
                     </div>
@@ -21,9 +22,18 @@
                 @endforeach
             @endcan
         </div>
-        <div class="ml-20 w-85">
+        <div id="cardMenu2" class="mobile-ml-0 mobile-card-menu2 mobile-w-100 ml-20 w-85">
             @yield('show')
         </div>
     </div>
+
+    <script>
+        @if(Request::is('course/*'))
+            $(document).ready(function(){
+                $("#cardMenu").animate({right: '100%'})
+                $("#cardMenu2").animate({left: '0'})
+            });
+        @endif
+    </script>
 @endsection
     
