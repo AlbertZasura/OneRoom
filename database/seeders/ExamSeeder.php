4da88759422a67df584b43b3b4202e2804e70fe5
kadek-ar <ikadek@indotrding.com>
2021-12-04 13:20:06 +0700

    Update ExamSeeder.php

--- database/seeders/ExamSeeder.php
+++ database/seeders/ExamSeeder.php
@@ -19,11 +19,119 @@ class ExamSeeder extends Seeder
             DB::table('exams')->insert(
                 [
                     [
-                        'title' => 'UTS Matematika',
+                        'title' => 'UTS Matematika Diskrit',
                         'type' => 'ujian tengah semester',
                         'start_date' => now()->toDateTimeString(),
                         'end_date' => '2021-09-26 15:10:12',
-                        'file' => 'soal_mtk.pdf',
+                        'file' => asset('img/Logo-OneRoom.png'),
+                        "user_id" => 1,
+                        "class_id" => 1,
+                        "course_id" => 1,
+                        "created_at" =>  now()->toDateTimeString(),
+                        "updated_at" => now()->toDateTimeString(),
+                    ],
+                    [
+                        'title' => 'Ulangan Sistem Pencernaan',
+                        'type' => 'ulangan',
+                        'start_date' => now()->toDateTimeString(),
+                        'end_date' => '2021-10-26 15:10:12',
+                        'file' => asset('img/Logo-OneRoom.png'),
+                        "user_id" => 1,
+                        "class_id" => 2,
+                        "course_id" => 4,
+                        "created_at" =>  now()->toDateTimeString(),
+                        "updated_at" => now()->toDateTimeString(),
+                    ],
+                    [
+                        'title' => 'UAS English Conversation',
+                        'type' => 'ujian akhir semester',
+                        'start_date' => now()->toDateTimeString(),
+                        'end_date' => '2021-11-26 15:10:12',
+                        'file' => asset('img/Logo-OneRoom.png'),
+                        "user_id" => 1,
+                        "class_id" => 3,
+                        "course_id" => 3,
+                        "created_at" =>  now()->toDateTimeString(),
+                        "updated_at" => now()->toDateTimeString(),
+                    ],
+                    [
+                        'title' => 'UTS Biologi Pengenalan',
+                        'type' => 'ujian tengah semester',
+                        'start_date' => now()->toDateTimeString(),
+                        'end_date' => '2021-09-26 15:10:12',
+                        'file' => asset('img/Logo-OneRoom.png'),
+                        "user_id" => 1,
+                        "class_id" => 4,
+                        "course_id" => 4,
+                        "created_at" =>  now()->toDateTimeString(),
+                        "updated_at" => now()->toDateTimeString(),
+                    ],
+                    [
+                        'title' => 'Ulangan Fisika Hukum Newton',
+                        'type' => 'ulangan',
+                        'start_date' => now()->toDateTimeString(),
+                        'end_date' => '2021-10-26 15:10:12',
+                        'file' => asset('img/Logo-OneRoom.png'),
+                        "user_id" => 1,
+                        "class_id" => 5,
+                        "course_id" => 5,
+                        "created_at" =>  now()->toDateTimeString(),
+                        "updated_at" => now()->toDateTimeString(),
+                    ],
+                    [
+                        'title' => 'UAS Fisika Pesawat Sederhana',
+                        'type' => 'ujian akhir semester',
+                        'start_date' => now()->toDateTimeString(),
+                        'end_date' => '2021-11-26 15:10:12',
+                        'file' => asset('img/Logo-OneRoom.png'),
+                        "user_id" => 1,
+                        "class_id" => 6,
+                        "course_id" => 5,
+                        "created_at" =>  now()->toDateTimeString(),
+                        "updated_at" => now()->toDateTimeString(),
+                    ],
+                    [
+                        'title' => 'UTS Aljabar Linear',
+                        'type' => 'ujian tengah semester',
+                        'start_date' => now()->toDateTimeString(),
+                        'end_date' => '2021-09-26 15:10:12',
+                        'file' => asset('img/Logo-OneRoom.png'),
+                        "user_id" => 1,
+                        "class_id" => 1,
+                        "course_id" => 1,
+                        "created_at" =>  now()->toDateTimeString(),
+                        "updated_at" => now()->toDateTimeString(),
+                    ],
+                    [
+                        'title' => 'Ulangan Mutasi Genetik dan Perkambangbiakan',
+                        'type' => 'ulangan',
+                        'start_date' => now()->toDateTimeString(),
+                        'end_date' => '2021-10-26 15:10:12',
+                        'file' => asset('img/Logo-OneRoom.png'),
+                        "user_id" => 1,
+                        "class_id" => 2,
+                        "course_id" => 4,
+                        "created_at" =>  now()->toDateTimeString(),
+                        "updated_at" => now()->toDateTimeString(),
+                    ],
+                    [
+                        'title' => 'UAS Reaksi Kimia dan molekul hidrogen',
+                        'type' => 'ujian akhir semester',
+                        'start_date' => now()->toDateTimeString(),
+                        'end_date' => '2021-11-26 15:10:12',
+                        'file' => asset('img/Logo-OneRoom.png'),
+                        "user_id" => 1,
+                        "class_id" => 3,
+                        "course_id" => 6,
+                        "created_at" =>  now()->toDateTimeString(),
+                        "updated_at" => now()->toDateTimeString(),
+                    ],
+                    [
+                        'title' => 'UTS Penggunaan Kata Sambung',
+                        'type' => 'ujian tengah semester',
+                        'start_date' => now()->toDateTimeString(),
+                        'end_date' => '2021-09-26 15:10:12',
+                        'file' => asset('img/Logo-OneRoom.png'),
                         "user_id" => 1,
                         "class_id" => 1,
                         "course_id" => 2,
@@ -31,11 +139,11 @@ class ExamSeeder extends Seeder
                         "updated_at" => now()->toDateTimeString(),
                     ],
                     [
-                        'title' => 'Ulangan IPA',
+                        'title' => 'Ulangan Penarapan kata majemuk',
                         'type' => 'ulangan',
                         'start_date' => now()->toDateTimeString(),
                         'end_date' => '2021-10-26 15:10:12',
-                        'file' => 'soal_ipa.pdf',
+                        'file' => asset('img/Logo-OneRoom.png'),
                         "user_id" => 1,
                         "class_id" => 2,
                         "course_id" => 2,
@@ -43,27 +151,26 @@ class ExamSeeder extends Seeder
                         "updated_at" => now()->toDateTimeString(),
                     ],
                     [
-                        'title' => 'UAS IPS',
+                        'title' => 'UAS Ion Molekul Hidrogen Dan Molekul Hidrogen',
                         'type' => 'ujian akhir semester',
                         'start_date' => now()->toDateTimeString(),
                         'end_date' => '2021-11-26 15:10:12',
-                        'file' => 'soal_ips.pdf',
+                        'file' => asset('img/Logo-OneRoom.png'),
                         "user_id" => 1,
                         "class_id" => 3,
-                        "course_id" => 3,
+                        "course_id" => 6,
                         "created_at" =>  now()->toDateTimeString(),
                         "updated_at" => now()->toDateTimeString(),
-                    ]
-                ]);
-                
-                
+                    ],
+                ]
+            );
 
-                for ($i=0; $i < 3 ; $i++) { 
-                    for ($j=30; $j>$i+1 ; $j--) { 
-                        $class=Exam::find($i+1);
-                        $class->users()->attach($j);
-                    }
+            for ($i=0; $i < 12 ; $i++) { 
+                for ($j=30; $j>$i+1 ; $j--) { 
+                    $class=Exam::find($i+1);
+                    $class->users()->attach($j);
                 }
+            }
 
     }
 }
