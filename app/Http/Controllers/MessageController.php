5b0da0db3a434bf9e3f3deca0c3f099bd3a49756
helenaawijaya <74819954+helenaawijaya@users.noreply.github.com>
2021-12-19 14:12:32 +0700

    mesage_controller

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -111,7 +111,6 @@ class MessageController extends Controller
      */
     public function destroy(Message $message)
     {
-        dd($message);
         $message->delete();
         Alert::success('Berhasil', 'Pengumuman berhasil dihapus!');
         return redirect()->route('messages.index');
